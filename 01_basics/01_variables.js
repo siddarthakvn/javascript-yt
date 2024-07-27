e1c07c1a094efb90c94885e463f3dddaebc7667b
siddarthakvn <160315214+siddarthakvn@users.noreply.github.com>
2024-07-27 07:38:52 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId =144553
+let accountEmail="siddarthakvn@gmail.com"
+var accountPassword="12345"
+accountCity="Hyderabad"
+let accountState;
+
+// accountId= 2 
+// accountEmail="eyeyey@gmail.com"
+// accountPassword="fnf44"
+// accountCity="mumbai"
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+prefer not to use var 
+because of issue in block scope in functional scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+ "use strict"; //treat all js code as newer version
+
+ //alert("3+3") we are using node js not browser so error pops out 
+
+ console.log(3+3)
+ console.log("siddartha")
+
+ //number=> 2 to power 53
+ //bigint 
+//string => ""
+//bolean=true/false
+//null =>standalone value
+//undefined =>
+//symbol=> uniqueness
+
+//object 
+
+console.log(typeof null)
